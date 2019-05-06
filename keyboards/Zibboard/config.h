c3048c21568b9e467e356e141f675f536e442da0
ZibbeZabbe <ZibbeZabbe@users.noreply.github.com>
2019-05-06 05:28:06 +0200

    Add files via upload

--- keyboards/Zibboard/config.h
+++ keyboards/Zibboard/config.h
@@ -41,11 +41,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS { D2, D3, D9, D4, D5, D6, D7, D8  }
-#define MATRIX_COL_PINS { D16, D14, D15, D18, D19, D20, D21 }
+#define MATRIX_ROW_PINS { D1, D0, B5, D4, C6, D7, E6, B4  }
+#define MATRIX_COL_PINS { B2, B3, B1, F7, F6, F5, F4 }
 #define UNUSED_PINS
 
-/* COL2ROW, ROW2COL{ D21, D20, D19, D18, D15, D14, D16 }*/
 #define DIODE_DIRECTION COL2ROW
 
 /*
--- keyboards/Zibboard/keymaps/default/keymap.c
+++ keyboards/Zibboard/keymaps/default/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 
+
 // Layers
 #define DEFALT 0
 //#define LOWER  1
@@ -7,7 +8,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [DEFALT] = LAYOUT(
-    KC_A, KC_B, KC_C, KC_D, LT(LAYER_1, KC_E),           \
+    KC_A, KC_B, KC_C, KC_D, LT(RESETL, KC_E),           \
     KC_F, KC_G, KC_H, KC_I, KC_J,           \
     KC_K, KC_L, KC_M, KC_N, KC_O,           \
     KC_P, KC_Q, KC_R, KC_S, KC_T,           \
@@ -18,23 +19,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
 
-//  [LOWER] = LAYOUT(
-//  RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-//  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-//  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-//  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-//  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-//	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-//	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-//	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO \
-//  ),
-
+/*   [LOWER] = LAYOUT(
+ *  RESET, KC_NO, KC_NO, KC_NO, KC_NO, 				\
+ *   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,				 \
+ *   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,				 \
+ *  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 				\
+ *   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+ * 	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+ *	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+ * 	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO \
+ *   ),
+ */
 
   [RESETL] = LAYOUT(
-    RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    RESET, KC_NO, KC_NO, KC_NO, KC_NO, 				 \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 				 \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 				 \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 				 \
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
@@ -43,49 +44,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 //-----------------------------------------------------------------------------
-
-const uint16_t PROGMEM fn_actions[] = {
-  [0] = ACTION_LAYER_MOMENTARY(RAISE), // Raise layer
-  [1] = ACTION_LAYER_MOMENTARY(LOWER), // Lower layer
-  [2] = ACTION_LAYER_TOGGLE(BDO), // BDO layer
-  [3] = ACTION_LAYER_TOGGLE(RESETL) // RESET layer
-};
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  // MACRODOWN only works in this function
-  switch (id) {
-  case 0:
-    if (record->event.pressed) {
-      register_code(KC_RSFT);
-    }
-    else {
-      unregister_code(KC_RSFT);
-    }
-    break;
-  }
-  return MACRO_NONE;
-};
-
-static uint8_t qw_dv_swap_state = 0;
-
-bool process_record_user (uint16_t keycode, keyrecord_t *record) {
-  if (keycode == KC_LGUI) {
-    if (record->event.pressed) {
-      qw_dv_swap_state |= 0b00000001;
-    } else {
-      qw_dv_swap_state &= ~(0b00000001);
-    }
-  }
-  if (keycode == KC_LCTL) {
-    if (record->event.pressed) {
-      qw_dv_swap_state |= 0b00000010;
-    } else {
-      qw_dv_swap_state &= ~(0b00000010);
-    }
-  }
-
-  if (qw_dv_swap_state == 0b00000011) {
-    layer_invert(DVORAK);
-  }
-  return true;
-}
